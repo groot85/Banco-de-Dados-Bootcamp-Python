8cf66aab3b7c8d67c6007cc526db5fa4c0f88797
groot85 <68751054+groot85@users.noreply.github.com>
2023-09-27 14:09:39 +0000

    Exercicios atualizados com teoria e resoluçao

--- main.py
+++ main.py
@@ -86,20 +86,84 @@ for alunos in res:
     print(alunos)
 
 # 5. Criar uma Tabela e Inserir Dados
-# Crie uma tabela chamada "clientes" com os campos: id (chave
-# primária), nome (texto), idade (inteiro) e saldo (float). 
-#Insira alguns registros de clientes na tabela.
+# Crie uma tabela chamada "clientes" com os campos: id (chave primária), nome (texto), idade (inteiro) e saldo (float). 
+
+# cur.execute ("CREATE TABLE IF NOT EXISTS clientes (id INTEGER primary key, nome VARCHAR(250),idade INTEGER, saldo FLOAT);")
+
+# #Insira alguns registros de clientes na tabela.
+# cur.execute("INSERT INTO clientes (id,nome,idade, saldo)"
+#               "VALUES (1,'Cesar',24, 1000)")
+# cur.execute("INSERT INTO clientes (id,nome,idade, saldo)"
+#               "VALUES (2,'Jujuba',38,3000)")
+# cur.execute("INSERT INTO clientes (id,nome,idade, saldo)"
+#               "VALUES (3,'Clair',13, 1000)")
+# cur.execute("INSERT INTO clientes (id,nome,idade, saldo)"
+#               "VALUES (4,'Mary',12,5000)")
+# cur.execute("INSERT INTO clientes (id,nome,idade, saldo)"
+#                "VALUES (5,'Mel',8,500)")
+# cur.execute("INSERT INTO clientes (id,nome,idade, saldo)"
+#                 "VALUES (6,'Ero',60, 7000)")
+# cur.execute("INSERT INTO clientes (id,nome,idade, saldo)"
+#                 "VALUES (7,'Alci',61,7000)")
+# cur.execute("INSERT INTO clientes (id,nome,idade, saldo)"
+#                 "VALUES (8,'Lau',38,8000)")
+# cur.execute("INSERT INTO clientes (id,nome,idade, saldo)"
+#                 "VALUES (9,'Fabricio',43,900)")
+# cur.execute("INSERT INTO clientes (id,nome,idade, saldo)"
+#                 "VALUES (10,'Anna',19,700)")
+#testando a tabela
+print("\n"'Os registros da tabela clientes são:'"\n")
+res = cur.execute("SELECT * FROM clientes")
+for clientes in res:
+    print(clientes)
 
 # 6. Consultas e Funções Agregadas
+#materiais pesquisados: https://www.devmedia.com.br/sql-max-min-avg-sum-e-count/41218
+
 # Escreva consultas SQL para realizar as seguintes tarefas:
 # a) Selecione o nome e a idade dos clientes com idade superior a 30 anos.
+print("\n"'Os clientes com mais de 30 anos:'"\n")
+res = cur.execute("SELECT nome, idade FROM clientes WHERE idade>30")
+for clientes in res:
+    print(clientes)
+
 # b) Calcule o saldo médio dos clientes.
+print("\n"'O saldo médio dos clientes é:'"\n")
+res = cur.execute("SELECT AVG (saldo) as SALDO_MEDIO FROM clientes")
+for clientes in res:
+    print(clientes)
+
 # c) Encontre o cliente com o saldo máximo.
+print("\n"'O cliente com saldo maior de todos é:'"\n")
+res = cur.execute("SELECT C.nome as Rico, MAX(saldo) as MAIOR_SALDO FROM clientes C ")
+for clientes in res:
+    print(clientes)
+
 # d) Conte quantos clientes têm saldo acima de 1000.
+cur.execute("SELECT count(1) FROM clientes WHERE saldo >1000")
+count = list(cur)[0]
+print("\n" "A quantidade de clientes com saldo acima de 1000 é: %d" %(count))
+
+print("\n"'Os clientes com saldo acima de mil são:'"\n")
+res = cur.execute("SELECT nome, saldo FROM clientes WHERE saldo >1000")
+for clientes in res:
+    print(clientes)
+
 
 # 7. Atualização e Remoção com Condições
 # a) Atualize o saldo de um cliente específico.
+print("\n"'O novo saldo da Ana é:'"\n")
+res = cur.execute("UPDATE clientes SET saldo=1300 WHERE id=10")
+res = cur.execute("SELECT nome,saldo FROM clientes WHERE id=10")
+for clientes in res:
+    print(clientes)
+
 # b) Remova um cliente pelo seu ID.
+print("\n"'A lista atualizada de clientes excluindo o 9 é:'"\n")
+res = cur.execute("DELETE FROM clientes WHERE id=9")
+res = cur.execute("SELECT * FROM clientes")
+for clientes in res:
+    print(clientes)
 
 # 8. Junção de Tabelas
 # Crie uma segunda tabela chamada "compras" com os campos: id
